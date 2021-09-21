c75436429a259d52efe232c87847fb74db204c97
YANNIS_DUPONT <yan.dupont60@gmail.com>
2021-09-21 11:35:12 +0200

    first commit

--- ConsoleApplication10/ConsoleApplication10/ConsoleApplication10.cpp
+++ ConsoleApplication10/ConsoleApplication10/ConsoleApplication10.cpp
@@ -6,4 +6,21 @@ int main()
 
 {
 	int a, b, c;
+	printf("donnez 3 entiers:\n");
+	scanf("%d%d%d", &a, &b, &c);
+	if (a >= b && a >= c)
+	{
+     if (b >= c) printf("%d %d %d\n", c, b, a);
+	else printf("%d %d %d\n", c, b, a);
+	}
+		
+	if (c >= a && c >= b)
+	{
+    if (a >= a && c >= b) printf("%d %d %d\n", b, a, c);
+		else printf ("%d %d %d\n", b, a, c")
+	}
+		
+	system("pause");
+	return 0;
+	
 }
